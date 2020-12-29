290db329687cf9f4e765aa47925b564a34faa4e3
Yuliang <xiaoliublog@gmail.com>
2020-12-29 14:03:28 +0800

    display the records

--- pages/overtime.tsx
+++ pages/overtime.tsx
@@ -1,14 +1,29 @@
-import React, { useMemo, useState } from 'react';
+import React, { FormEvent, useMemo, useState, useEffect } from 'react';
 import Head from "next/head";
 import Store from "../utils/client/Store";
+import dayjs from 'dayjs';
 
 
 export default function Overtime() {
   const db = useMemo(() => Store.getCollection("overtime"), []);
-  const [time, setTime] = useState<string>("");
 
-  const saveTime = () => {
-    db.insert({ time })
+  const [all, setAll] = useState([]);
+  const [startTime, setStartTime] = useState<string>(dayjs().format("YYYY-MM-DDTHH:mm"));
+  const [endTime, setEndTime] = useState<string>(dayjs().format("YYYY-MM-DDTHH:mm"));
+
+  const refreshRecords = async () => {
+    const records = await db.find({}).toArray()
+    setAll(records);
+  }
+
+  useEffect(() => {
+    if (db) refreshRecords()
+  }, [db])
+
+  const saveTime = async (e: FormEvent) => {
+    e.preventDefault();
+    await db.insert({ startTime, endTime });
+    refreshRecords();
   }
 
   return (
@@ -17,8 +32,16 @@ export default function Overtime() {
         <title>Tools for 996 - Overtime</title>
       </Head>
       <div>Work for overtime</div>
-      <input type="datetime-local" value={time} onChange={e => setTime(e.target.value)} />
-      <button onClick={saveTime} >save</button>
+
+      {all.map(r => (
+        <div key={r._id}>{`${r.startTime} to ${r.endTime}`}</div>
+      ))}
+
+      <form onSubmit={saveTime}>
+        <input type="datetime-local" required value={startTime} onChange={e => setStartTime(e.target.value)} />
+        <input type="datetime-local" required value={endTime} onChange={e => setEndTime(e.target.value)} />
+        <button type="submit" >save</button>
+      </form>
     </div>
   );
 }
